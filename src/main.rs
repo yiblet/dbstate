1e0352579965df8ea25e92406edcbec51d4f78ea
Shalom Yiblet <shalom.yiblet@gmail.com>
2022-10-01 21:53:05 -0700

    feat: add initial table

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use sqlx::PgPool;
+
+mod schema;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let pool = PgPool::connect(&env::var("DATABASE_URL")?).await?;
+
+    let tables = schema::table::get_all_tables(&pool).await;
+
+    println!("{:?}", tables);
+
+    Ok(())
 }
